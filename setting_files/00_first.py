f200205cc50caca8abcc61e1fb39cffc3ed18d33
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2023-11-24 15:38:33 +0900

    runのマジックコマンドをやはり設定　

--- setting_files/00_first.py
+++ setting_files/00_first.py
@@ -1,2 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+from IPython.core.magic import register_line_magic
+@register_line_magic
+def r(line):
+  get_ipython().run_line_magic('run', ' -i ' + line)
+del r
