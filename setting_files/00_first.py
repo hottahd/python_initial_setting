1b4e44c0ff28aff960c7b1b810833480d6ae3602
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2023-11-28 09:58:10 +0900

    pltの一部は暗黙にimportすることにした

--- setting_files/00_first.py
+++ setting_files/00_first.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from matplotlib.pyplot import plot, pcolormesh, clf
+
 from IPython.core.magic import register_line_magic
 @register_line_magic
 def r(line):
